e7f70005b23355f15aefceaf94cd1e5bd4f9de91
wang90 <393210564@qq.com>
2021-06-12 01:28:27 +0800

    v0.0.1 (#1)
    * refactor: 💡 create projection
    
    * docs: ✏️ README
    
    ✅ Closes: git push
    
    * feat: 🎸 add home page

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <home></home>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/home.vue'
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    Home,
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
