4191529f39184224432c1abdd840627d54205775
wang90 <393210564@qq.com>
2021-06-13 00:50:27 +0800

    refactor: 💡 projection config (#2)
    * refactor: 💡 create projection
    
    * docs: ✏️ README
    
    ✅ Closes: git push
    
    * feat: 🎸 add home page
    
    * perf: ⚡️ add model
    
    * ci: 🎡 add ui theme
    
    * refactor: 💡 vite.config add @src

---
+++ src/components/EditRich.vue
@@ -0,0 +1,18 @@
+<template>
+    <div> edit-rich-component </div>
+</template>
+  
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+    name: 'EditRich',
+    setup: () => {
+      
+    }
+})
+</script>
+  
+<style scoped>
+
+</style>
+  
